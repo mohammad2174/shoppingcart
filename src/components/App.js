6ecd89146f09e19429f23bb88025975e4935f6e2
mohammad2174 <>
2021-10-23 16:22:50 +0330

    completed install tailwind css and header section

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="lg:flex lg:items-center lg:justify-between">
+      <div className="flex-1 min-w-0">
+        <h2 className="p-4 bg-blue-400 text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">Shopping Card</h2>
+        <hr />
+      </div>
+    </div>
+  );
+}
+
+export default App;
