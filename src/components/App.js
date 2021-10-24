da89718fe31c9b9e613971769363afc6e9df69bf
mohammad2174 <>
2021-10-24 13:29:01 +0330

    recieved products

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
+import React, { Component } from "react";
+import shop from "../api/shop";
 
-function App() {
-  return (
-    <div className="lg:flex lg:items-center lg:justify-between">
-      <div className="flex-1 min-w-0">
-        <h2 className="p-4 bg-blue-400 text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">Shopping Card</h2>
-        <hr />
+class App extends Component {
+  state = {
+    products : []
+  }
+  componentWillMount() {
+    shop.getProducts((products) => {
+      console.log(products);
+      this.setState({
+        products
+      })
+    })
+  }
+  render() {
+    return (
+      <div className="lg:flex lg:items-center lg:justify-between">
+        <div className="flex-1 min-w-0">
+          <h2 className="p-4 bg-blue-400 text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">Shopping Card</h2>
+          <hr />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
