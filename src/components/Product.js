d511c7537e6e424d291f85d0b0f626db26f56f6a
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2021-10-27 10:50:57 +0330

    fixed tags <a> on the shopping-cart

--- src/components/Product.js
+++ src/components/Product.js
@@ -17,10 +17,8 @@ class Product extends Component {
           <div className="mt-4 flex justify-between">
             <div>
               <h3 className="text-sm text-gray-700">
-                <a href="#">
                   <span aria-hidden="true" className="absolute inset-0" />
                   {title} x {inventory}
-                </a>
               </h3>
               <p className="mt-1 text-sm text-gray-500">{color}</p>
             </div>
