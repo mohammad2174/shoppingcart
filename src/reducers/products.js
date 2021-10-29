ef544248d2b37c8d83f14ad697a85cff6936c12b
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2021-10-29 15:07:47 +0330

    deploy redux-logger

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,6 +1,18 @@
+import { RECIEVE_PRODUCTS } from "../constants/actionTypes";
+
 const products = (state = {}, action) => {
     console.log(action);
     switch (action.type) {
+        case RECIEVE_PRODUCTS:
+            let newProducts = action.type.reduce((obj, product) => {
+                obj[product.id] = product;
+                return obj;
+            }, {})
+
+            return {
+                ...state,
+                ...newProducts
+            }
 
         default: 
         return state
