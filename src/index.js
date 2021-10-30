3ae8388421bb1aa1bf7c4f562fd0ff73e52ab906
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2021-10-30 16:50:54 +0330

    release createLogger

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import reducers from './reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { createLogger } from 'redux-logger';
 
-const store = createStore(reducers, composeWithDevTools());
+const middleware = [];
+if(process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger())
+}
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(...middleware)));
 
 ReactDOM.render(
   <Provider store={store}>
