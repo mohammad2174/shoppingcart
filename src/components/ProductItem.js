e56ea92184ac285ed3b1530000e92255c1afbbb0
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2021-11-01 11:06:50 +0330

    In Progress section Add To Card, worked on action & reducer

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -4,12 +4,12 @@ import Product from "./Product";
 class ProductItem extends Component {
 
   render() {
-      const { product } = this.props;
+      const { product , onAddToCardClicked } = this.props;
       
     return (
       <div>
           <Product {...product} />
-           <a href="#" disabled = {product.inventory ? 'Add To Card' : 'Sold Out'} className="flex justify-center items-center mt-12 px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+           <a onClick={onAddToCardClicked} disabled = {product.inventory ? 'Add To Card' : 'Sold Out'} className="flex justify-center items-center mt-12 px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
            {product.inventory > 0 ? 'Add To Card' : 'Sold Out'}
            </a>
       </div>  
