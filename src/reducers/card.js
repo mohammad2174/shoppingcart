ce5548e7bb13c2f980779d1471c8d3c5d63ed5bf
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2021-11-06 13:11:50 +0330

    completed card for the section reducers

---
+++ src/reducers/card.js
@@ -0,0 +1,37 @@
+import { ADD_TO_CARD } from "../constants/actionTypes";
+
+
+const initialState = {
+    addedIds : [],
+    quantityById : {}
+}
+
+const addedIds = (state = initialState.addedIds, action) => {
+    if(state.indexOf(action.productId) !== -1) {
+        return state;
+    }
+    return [...state, action.productId]
+}
+
+const quantityById = (state = initialState.quantityById, action) => {
+    const { productId } = action;
+    return {
+        ...state,
+        [productId] : (state[productId] || 0) + 1
+    }
+}
+
+const card = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_TO_CARD: 
+            return {
+                addedIds: addedIds(state.addedIds, action),
+                quantityById: quantityById(state.quantityById, action)
+            }
+        default:
+            return state;
+    }
+}
+
+
+export default card;
