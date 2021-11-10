93d788daeae4345a76e3042445836a8a40f34f1d
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2021-11-11 00:06:14 +0330

    create Header component & created basket for more details products

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { recieveProducts } from "../actions";
 import shop from "../api/shop";
-import ProductList from "./ProductList";
+import Cardlist from './Cardlist';
+import Header from "./Header";
+
 
 class App extends Component {
 
@@ -11,13 +13,28 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="lg:flex lg:items-center lg:justify-between">
-        <div className="flex-1 min-w-0">
-          <h2 className="p-4 bg-blue-400 text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">Shopping Card</h2>
-          <hr />
-          <ProductList />
+      <>
+      <Header />
+      <div className="bg-indigo-600">
+      <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between flex-wrap">
+          <div className="w-0 flex-1 flex items-center">
+            <p className="ml-3 font-medium text-white truncate">
+              <span className="md:hidden">please checkout your basket for the selected products</span>
+              <span className="hidden md:inline">please checkout your basket for the selected products</span>
+            </p>
+          </div>
+          <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+            <button
+              className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
+            >
+              <Cardlist products />
+            </button>
+          </div>
+        </div>
         </div>
       </div>
+      </>
     );
   }
 }
--- src/components/Cardlist.js
+++ src/components/Cardlist.js
@@ -13,7 +13,7 @@ class Cardlist extends Component {
     console.log(products);
     return (
       <div>
-           <button onClick={() => this.setOpen(true)} className="flex justify-center items-center w-full mt-12 px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+           <button onClick={() => this.setOpen(true)} className="font-medium">
             Basket
            </button>
            {
