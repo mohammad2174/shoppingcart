4fe7b5be0447239b74ca877e5cc5a2eb35e870a0
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2021-12-05 22:03:15 +0330

    adding svg icon to the Component Cardlist on the Button tag

--- src/components/Cardlist.js
+++ src/components/Cardlist.js
@@ -8,13 +8,12 @@ class Cardlist extends Component {
       setOpen = () => this.setState(prevState => ({
         setopen : !prevState.setopen
       }))
-  render() {
+  render(){
       const { products } = this.props;
-    console.log(products);
     return (
       <div>
-           <button onClick={() => this.setOpen(true)} className="font-medium">
-            Basket
+           <button onClick={() => this.setOpen()}>
+           {this.state.setopen ? <img src="https://img.icons8.com/ios-filled/30/000000/lock.png" alt="lock"/> : <img src="https://img.icons8.com/ios-filled/30/000000/unlock.png" alt="unlock"/>}
            </button>
            {
                 (this.state.setopen) ? <Card {...products} open={this.state.setopen} /> : ''
