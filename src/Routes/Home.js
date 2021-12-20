8a28884ef910201c426aff8cf6f17b7fd697639d
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2021-12-20 14:03:11 +0330

    minor works for the certain components

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
-import { connect } from "react-redux";
-import { recieveProducts } from "../actions";
-import shop from "../api/shop";
 import Cardlist from '../components/Cardlist';
 
 
 class Home extends Component {
-  // componentWillMount() {
-  //   shop.getProducts((products) => this.props.recieveProducts(products))
-  // }
+
     render () {
         return (
             <div className="bg-purple-600">
@@ -16,7 +11,7 @@ class Home extends Component {
               <div className="flex items-center justify-between flex-wrap">
                 <div className="w-0 flex-1 flex items-center">
                   <p className="ml-3 font-medium text-white truncate">
-                    <span className="md:hidden">please checkout your basket for the selected products</span>
+                    <span>please checkout your basket for the selected products</span>
                   </p>
                 </div>
                 <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
@@ -35,9 +30,5 @@ class Home extends Component {
     }
 }
 
-// const mapDispatchToProps = dispatch => ({
-//     recieveProducts : products => dispatch(recieveProducts(products))
-// })
 
-// export default connect(null, mapDispatchToProps)(Home);
 export default Home;
