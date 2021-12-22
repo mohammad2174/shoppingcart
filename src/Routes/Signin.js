7596ae9458bba960ff6104c89b27e0e73a5a25c7
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2021-12-22 11:53:39 +0330

    Link Register Component to Signin Component

--- src/Routes/Signin.js
+++ src/Routes/Signin.js
@@ -11,7 +11,7 @@ class SignIn extends Component {
               <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
               <p className="mt-2 text-center text-sm text-gray-600">
                 Or{' '}
-                <Link to="register">
+                <Link to="/register">
                 <span className="font-medium text-gray-600 cursor-pointer hover:text-indigo-500">
                 you don't have an any account <b className="text-indigo-500">Register</b>
                 </span>
