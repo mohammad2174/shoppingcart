16009572ed9798ded77c92b42dd9ce412ebc350e
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2021-12-24 15:35:52 +0330

    Change Header Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 import ProductList from "./ProductList";
+import { Link } from "react-router-dom";
 
 
 class Header extends Component {
 
   render() {
     return (
-        <div className="lg:flex lg:items-center lg:justify-between">
+        <div className="lg:flex lg:justify-between">
         <div className="flex-1 min-w-0">
-          <h2 className="p-4 bg-indigo-500 text-2xl font-bold leading-7 text-white sm:text-3xl sm:truncate">Shopping Card</h2>
+          <div className="py-1.5 pl-2 bg-gray-900 font-normal leading-7 text-white sm:text-lg sm:truncate">
+            <div className="flex flex-row justify-between">
+              <span>Shop Snazzy</span>
+              <div className="pr-3.5 flex flex-row justify-between">
+              <Link to="signin">  
+              <span className="pr-4">Sign in</span>
+              </Link>
+              <Link to="/register">
+              <span>Create an account</span>
+              </Link>
+              </div>
+            </div>
+          </div>
           <hr />
           <ProductList />
         </div>
