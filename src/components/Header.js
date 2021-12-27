4f6fc583bc0a351282cd15f8a5ecb53b23eb196d
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2021-12-27 16:37:43 +0330

    Style for Desktop main page

--- src/components/Header.js
+++ src/components/Header.js
@@ -62,46 +62,46 @@ class Header extends Component {
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
            <div className="max-w-2xl mx-auto py-16 sm:py-24 lg:py-32 lg:max-w-none">
           <h2 className="text-2xl font-extrabold text-gray-900">Shop by Catgory</h2>
-            <div class="mt-6 space-y-12 rounded-lg lg:space-y-0 grid grid-flow-col gap-3 lg:grid lg:grid-cols-2 lg:gap-x-5">
-              <div class="row-span-3">
+            <div class="mt-6 space-y-12 rounded-lg lg:space-y-0 grid grid-flow-col gap-0 lg:grid lg:grid-cols-2 lg:gap-x-5">
+              <div class="relative row-span-3">
+              <a href={callouts.href}>
               <img
                     src={callouts.imageSrc}
                     alt={callouts.imageAlt}
-                    className="w-full h-2/4 object-center object-cover"
+                    className="w-full h-3/5 cursor-pointer hover:opacity-75 rounded-lg object-center object-cover"
               />
-              <h3 className="mt-6 text-sm text-gray-500">
-                <a href={callouts.href}>
+              </a>
+              <h3 className="absolute bottom-1/2 mt-6 ml-3 text-sm text-gray-500">              
                   <span className="absolute inset-0" />
-                  {callouts.name}
-                </a>
+                  {callouts.name}               
               </h3>
-                <p className="text-base font-semibold text-gray-900">{callouts.description}</p>
+                <p className="absolute mb-6 ml-3 text-base bottom-1/2 font-semibold text-gray-900">{callouts.description}</p>
               </div>
                <div class="col-span-2">
+               <a href={improvement.href}>  
                <img
                     src={improvement.imageSrc}
                     alt={improvement.imageAlt}
-                    className="w-full h-1/4 object-center object-cover"
+                    className="w-full h-60 cursor-pointer hover:opacity-75 rounded-lg object-center object-cover"
                />
-                <h3 className="mt-6 text-sm text-gray-500">
-                  <a href={improvement.href}>
+               </a>
+                <h3 className="absolute bottom-2/3 mt-6 ml-3 text-sm text-gray-500">
                   <span className="absolute inset-0" />
                   {improvement.name}
-                  </a>
                 </h3>
-                <p className="text-base font-semibold text-gray-900">{callouts.description}</p>
+                <p className="absolute mb-6 ml-3 text-base bottom-2/3 font-semibold text-gray-900">{improvement.description}</p>
+                <a href={travel.href}>
                <img
                     src={travel.imageSrc}
                     alt={travel.imageAlt}
-                    className="w-full h-1/4 mt-2.5 object-center object-cover"
+                    className="w-full h-2/6 mt-2.5 cursor-pointer hover:opacity-75 rounded-lg object-center object-cover"
                />
-                <h3 className="mt-6 text-sm text-gray-500">
-                  <a href={travel.href}>
+               </a>
+                <h3 className="absolute bottom-1/2 mt-6 ml-3 text-sm text-gray-500">
                   <span className="absolute inset-0" />
                   {travel.name}
-                  </a>
                 </h3>
-                <p className="text-base font-semibold text-gray-900">{callouts.description}</p>
+                <p className="absolute mb-6 ml-3 text-base bottom-1/2 font-semibold text-gray-900">{travel.description}</p>
                </div>
                </div>
               </div>
