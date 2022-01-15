d3f602e03e48ef29f0221503490bc22b805ca1fa
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2022-01-15 16:29:24 +0330

    add modal to Component Mantops

--- src/Routes/Mantops.js
+++ src/Routes/Mantops.js
@@ -3,11 +3,14 @@ import { Dialog, RadioGroup, Transition } from '@headlessui/react';
 import { XIcon } from '@heroicons/react/outline';
 import { StarIcon } from '@heroicons/react/solid';
 import Navigation from "../components/Navigation";
+import tickLogo from '../assests/icons8-done.gif';
+
 
 
 class Tops extends Component {
   state = {
     open : false,
+    modalopen : false,
     top: '',
     selectedColor : [
       { name: 'White', class: 'bg-white', selectedClass: 'ring-gray-400' },
@@ -34,6 +37,14 @@ class Tops extends Component {
     this.setState({open : false})
   }
 
+  setmodalOpen = () => {
+    this.setState({modalopen : true, open : false})
+  }
+
+  setmodalClose = () => {
+    this.setState({modalopen : false})
+  }
+
   setSelectedColor = () => {
     this.setState(prevState => ({
       selectedColor : prevState.selectedColor 
@@ -58,7 +69,8 @@ class Tops extends Component {
               catimageSrc: 'https://images.unsplash.com/photo-1503340588524-222d094c7066?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8M3xkRUdXdnBIRWFwUXx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
               imageAlt: "Full Nelson.",
               describtion: 'Shop Now',
-              price: 46
+              price: 46,
+              inventory: 4
             },
             {
               id: 2,
@@ -68,7 +80,8 @@ class Tops extends Component {
               catimageSrc: 'https://images.unsplash.com/photo-1529374255404-311a2a4f1fd9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8Mnw5OTUzMDU5fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
               imageAlt: "Re-Arranged.",
               describtion: 'Shop Now',
-              price: 61
+              price: 61,
+              inventory: 3
             },
             {
               id: 3,
@@ -78,7 +91,8 @@ class Tops extends Component {
               catimageSrc: 'https://media.istockphoto.com/photos/blank-white-tshirt-front-with-clipping-path-picture-id482949611?b=1&k=20&m=482949611&s=170667a&w=0&h=oPsmQCKbaB4oiWTmQ2QCj485lb60y47FplQezKpkv-0=',
               imageAlt: "My Way.",
               describtion: 'Shop Now',
-              price: 74
+              price: 74,
+              inventory: 5
             },
             {
               id: 4,
@@ -88,7 +102,8 @@ class Tops extends Component {
               catimageSrc: 'https://images.unsplash.com/photo-1562157873-818bc0726f68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw5OTUzMDU5fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
               imageAlt: "Counterfeit.",
               describtion: 'Shop Now',
-              price: 37
+              price: 37,
+              inventory: 2
             },
           ]
         const product = {
@@ -102,7 +117,8 @@ class Tops extends Component {
             colors: this.state.selectedColor,
             sizes: this.state.selectedSize,
           }
-         console.log(this.state.selectedSize);   
+          const cancelButtonRef = this.state.setmodalClose
+ 
       return (
         <>
         <Navigation />
@@ -131,7 +147,10 @@ class Tops extends Component {
                     </h3>
                     <p className="mt-1 text-sm text-gray-500">{top.describtion}</p>
                   </div>
+                  <div>
                   <p className="text-sm font-medium text-gray-500">${top.price}</p>
+                  <p className="mt-1 text-sm text-gray-500">X{top.inventory}</p>
+                  </div>
                 </div>
               </div>
             ))}
@@ -219,7 +238,6 @@ class Tops extends Component {
                         Product options
                       </h3>
 
-                      <form>
                         {/* Colors */}
                         <div>
                           <h4 className="text-sm text-gray-900 font-medium">Color</h4>
@@ -318,13 +336,82 @@ class Tops extends Component {
                         <button
                           type="submit"
                           className="mt-6 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                          onClick={() => this.setmodalOpen()}
                         >
                           Add to bag
                         </button>
-                      </form>
                     </section>
                   </div>
                 </div>
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition.Root>
+      {/* modals */}
+      <Transition.Root show={this.state.modalopen} as={Fragment}>
+      <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" initialFocus={cancelButtonRef} onClose={this.setmodalOpen}>
+        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          {/* This element is to trick the browser into centering the modal contents. */}
+          <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+            &#8203;
+          </span>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          >
+            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <img src={tickLogo} aria-hidden="true"/>
+                  </div>
+                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                      Adding to bag
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        Are you sure you want to adding to your bag? If not, press the Cancel button.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <button
+                  type="button"
+                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                  onClick={() => this.setmodalClose()}
+                >
+                  Deactivate
+                </button>
+                <button
+                  type="button"
+                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                  onClick={() => this.setmodalClose()}
+                  ref={cancelButtonRef}
+                >
+                  Cancel
+                </button>
               </div>
             </div>
           </Transition.Child>
