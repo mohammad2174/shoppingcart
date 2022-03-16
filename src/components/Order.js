5ef1d3d2817ad834411eca317f22a829aac1112a
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2022-03-16 14:52:31 +0330

    creating minusSubmit method

--- src/components/Order.js
+++ src/components/Order.js
@@ -45,8 +45,7 @@ class Order extends Component {
       const firstexpiredate = this.state.expiredate.slice(-5, -3);
       const orders = this.props.orders.orders
       const checkouts = this.props.checkouts.checkouts
-      // console.log(this.state.expiredate.slice(-2));
-      console.log(orders);
+      console.log(date);
     return (
       <div className="max-w-7xl mt-16 mx-auto px-4 sm:px-6 lg:px-8">
         <h2 className="text-2xl font-extrabold text-gray-800">Order Details</h2>
@@ -70,6 +69,8 @@ class Order extends Component {
                       <p className="mt-4 text-sm text-gray-500">{checkout.size}</p>
                       <p className="mt-1 text-sm text-gray-500">{checkout.color}</p>
                     </div>
+                    {orders && orders.length ?
+                    <>
                     {orders.map((order) => (
                     <div className="grid grid-cols-1 gap-1 md:grid-cols-2">
                     <div className="relative md:row-span-1">
@@ -82,6 +83,8 @@ class Order extends Component {
                     </div>
                     </div>
                     ))}
+                    </>
+                    : ''}
                     <div className="mt-20">
                     <p className="mb-6 text-base font-extrabold text-gray-800">Preparing to ship on {date}</p>
                      <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
@@ -100,6 +103,8 @@ class Order extends Component {
               </ul> 
             ))}
             <div class="mt-6 rounded-lg grid grid-cols-1 gap-1 md:grid-cols-2">
+            {orders && orders.length ?
+            <>
             {orders.map((order) => (
               <>
               <div className="mt-7 relative md:row-span-1 ml-0 md:ml-6 grid grid-cols-1 gap-1 md:grid-cols-2">
@@ -132,6 +137,8 @@ class Order extends Component {
               </div>
               </>
               ))}
+            </>
+            : ''}
             </div>
       </div> 
     )
