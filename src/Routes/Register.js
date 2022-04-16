f719ca64222512bc1dfe853a6b531938ca5a16da
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2022-04-16 14:47:24 +0430

    working on the error messages

--- src/Routes/Register.js
+++ src/Routes/Register.js
@@ -30,27 +30,29 @@ class Register extends Component {
     loginFormData.append("email", this.state.email)
     loginFormData.append("password", this.state.password)
     axios.post("http://localhost:8000/api/v1/register", loginFormData)
-    .then((res) => {
-      this.setState({ redirect: true });
-      this.props.setCurrentUser({currentUser: {
-        password: this.state.password,
-        email: this.state.email,
-        id: res.data.data.id
-      }})
-    })
-    .catch((err) => {
-      this.setState({ errorMessage: err.response.data });
-      console.log(err.response.data);
-    });
+      .then((res) => {
+        this.setState({ redirect: true });
+        this.props.setCurrentUser({
+          currentUser: {
+            password: this.state.password,
+            email: this.state.email,
+            id: res.data.data.id
+          }
+        })
+      })
+      .catch((err) => {
+        this.setState({ errorMessage: err.response.data });
+        console.log(err.response.data);
+      });
   }
 
-    render() {
-      if(this.state.redirect) {
-        return <Navigate to="/checkout" />
-      }
-
-        return (
-        <>
+  render() {
+    if (this.state.redirect) {
+      return <Navigate to="/checkout" />
+    }
+    console.log(this.state.errorMessage);
+    return (
+      <>
         <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
           <div className="max-w-md w-full space-y-8">
             <div>
@@ -58,82 +60,84 @@ class Register extends Component {
               <p className="mt-2 text-center text-sm text-gray-600">
                 Or{' '}
                 <Link to="/signin">
-                <span className="font-medium text-gray-600 cursor-pointer hover:text-indigo-500">
-                Already A Member? <b className="text-indigo-500">Login</b>
-                </span>
+                  <span className="font-medium text-gray-600 cursor-pointer hover:text-indigo-500">
+                    Already A Member? <b className="text-indigo-500">Login</b>
+                  </span>
                 </Link>
               </p>
             </div>
             <div className="bg-white shadow-2xl overflow-hidden sm:rounded-lg">
-            <form className="w-10/12 m-9 space-y-6" onSubmit={this.handleSubmit}>
-              <input type="hidden" name="remember" defaultValue="true" />
-              <div className="rounded-md shadow-sm -space-y-px">
-              {this.state.errorMessage.email ? <p className="text-red-500">{this.state.errorMessage.email[0]}</p> : <p className="text-red-500">{this.state.errorMessage.data}</p>}
-                <div>
-                First name
-                  <input
-                    id="first-name"
-                    name="fname"
-                    autoComplete="fname"
-                    required
-                    onChange={this.handleChange}
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border-b-2 border-gray-400 text-gray-900 focus:outline-none focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  />
-                </div>  
+              <form className="w-10/12 m-9 space-y-6" onSubmit={this.handleSubmit}>
+                <input type="hidden" name="remember" defaultValue="true" />
+                <div className="rounded-md shadow-sm -space-y-px">
+                  {this.state.errorMessage.name ? <p className="text-red-500">{this.state.errorMessage.name[0]}</p> : <p className="text-red-500">{this.state.errorMessage.data}</p>}
+                  <div>
+                    First name
+                    <input
+                      id="first-name"
+                      name="fname"
+                      autoComplete="fname"
+                      onChange={this.handleChange}
+                      className={this.state.errorMessage.name || this.state.errorMessage.data ? "appearance-none rounded-none relative block w-full px-3 py-2 border-b-2 border-red-400 text-gray-900 focus:outline-none focus:border-indigo-500 focus:z-10 sm:text-sm" : "appearance-none rounded-none relative block w-full px-3 py-2 border-b-2 border-gray-400 text-gray-900 focus:outline-none focus:border-indigo-500 focus:z-10 sm:text-sm"}
+                    />
+                  </div>
 
-                <div className="pt-4">
+                  <div className="pt-4">
                     Last name
-                  <input
-                    id="last-name"
-                    name="lname"
-                    autoComplete="lname"
-                    required
-                    onChange={this.handleChange}
-                    className={this.state.errorMessage.email || this.state.errorMessage.data ? "appearance-none rounded-none relative block w-full px-3 py-2 border-b-2 border-red-400 text-gray-900 focus:outline-none focus:border-indigo-500 focus:z-10 sm:text-sm" : "appearance-none rounded-none relative block w-full px-3 py-2 border-b-2 border-gray-400 text-gray-900 focus:outline-none focus:border-indigo-500 focus:z-10 sm:text-sm"}
-                  />
+                    <input
+                      id="last-name"
+                      name="lname"
+                      autoComplete="lname"
+                      onChange={this.handleChange}
+                      className={this.state.errorMessage.name || this.state.errorMessage.data ? "appearance-none rounded-none relative block w-full px-3 py-2 border-b-2 border-red-400 text-gray-900 focus:outline-none focus:border-indigo-500 focus:z-10 sm:text-sm" : "appearance-none rounded-none relative block w-full px-3 py-2 border-b-2 border-gray-400 text-gray-900 focus:outline-none focus:border-indigo-500 focus:z-10 sm:text-sm"}
+                    />
+                  </div>
+
+                  <div className="pt-4">
+                    {this.state.errorMessage.email ? <p className="text-red-500">{this.state.errorMessage.email[0]}</p> : <p className="text-red-500">{this.state.errorMessage.data}</p>}
+                    <div>
+                      Email address
+                      <input
+                        id="email-address"
+                        name="email"
+                        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                        autoComplete="email"
+                        onChange={this.handleChange}
+                        className={this.state.errorMessage.email || this.state.errorMessage.data ? "appearance-none rounded-none relative block w-full px-3 py-2 border-b-2 border-red-400 text-gray-900 focus:outline-none focus:border-indigo-500 focus:z-10 sm:text-sm" : "appearance-none rounded-none relative block w-full px-3 py-2 border-b-2 border-gray-400 text-gray-900 focus:outline-none focus:border-indigo-500 focus:z-10 sm:text-sm"}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="pt-4">
+                    {this.state.errorMessage.password ? <p className="text-red-500">{this.state.errorMessage.password[0]}</p> : <p className="text-red-500">{this.state.errorMessage.data}</p>}
+                    <div>
+                      Password
+                      <input
+                        id="password"
+                        name="password"
+                        autoComplete="current-password"
+                        onChange={this.handleChange}
+                        className={this.state.errorMessage.password || this.state.errorMessage.data ? "appearance-none rounded-none relative block w-full px-3 py-2 border-b-2 border-red-400 text-gray-900 focus:outline-none focus:border-indigo-500 focus:z-10 sm:text-sm" : "appearance-none rounded-none relative block w-full px-3 py-2 border-b-2 border-gray-400 text-gray-900 focus:outline-none focus:border-indigo-500 focus:z-10 sm:text-sm"}
+                      />
+                    </div>
+                  </div>
                 </div>
 
-                <div className="pt-4">
-                    Email address
-                  <input
-                    id="email-address"
-                    name="email"
-                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-                    autoComplete="email"
-                    required
-                    onChange={this.handleChange}
-                    className={this.state.errorMessage.email || this.state.errorMessage.data ? "appearance-none rounded-none relative block w-full px-3 py-2 border-b-2 border-red-400 text-gray-900 focus:outline-none focus:border-indigo-500 focus:z-10 sm:text-sm" : "appearance-none rounded-none relative block w-full px-3 py-2 border-b-2 border-gray-400 text-gray-900 focus:outline-none focus:border-indigo-500 focus:z-10 sm:text-sm"}
-                  />
+                <div className="flex pt-12 items-center justify-between">
+                  <button
+                    type="submit"
+                    className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    Sign in
+                  </button>
                 </div>
-
-                <div className="pt-4">
-                    Password
-                  <input
-                    id="password"
-                    name="password"
-                    autoComplete="current-password"
-                    required
-                    onChange={this.handleChange}
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border-b-2 border-gray-400 text-gray-900 focus:outline-none focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  />
-                </div>
-              </div>  
-              
-              <div className="flex pt-12 items-center justify-between">
-                <button
-                  type="submit"
-                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                  Sign in
-                </button>
-              </div>
-            </form>
+              </form>
             </div>
           </div>
         </div>
-        </>
-        )
-    }
+      </>
+    )
+  }
 }
 
 
